8751626e1d9d46fe2b1db30e8efec90f65890e94
Loris <45939615+lchastanet@users.noreply.github.com>
2023-09-11 13:42:23 +0200

    add files

--- src/controllers/mailController.js
+++ src/controllers/mailController.js
@@ -1,18 +1,5 @@
-const { sendMail } = require("../helpers/mailer")
-const resetTemplate = require("../templates/resetTemplate")
-
 exports.forgotten = async (req, res) => {
   const recip = req.body.email
 
-  try {
-    const template = resetTemplate("http://localhost:8000/api/reset")
-
-    const info = await sendMail(recip, template)
-
-    console.log(info)
-
-    res.senStatus(200)
-  } catch (e) {
-    res.sendStatus(500)
-  }
+  res.json({ recip })
 }
